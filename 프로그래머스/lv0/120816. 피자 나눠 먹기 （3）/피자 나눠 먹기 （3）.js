7c32e5016cf920a42af33410d0447242dfb73761
soyoung03 <128986073+soyoung03@users.noreply.github.com>
2023-07-14 02:39:26 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,13 +1,5 @@
 function solution(slice, n) {
     var answer = 0;
-    if(slice / n < 1) {
         answer = Math.ceil(n/slice);
-    } 
-    else if(slice / n > 1){
-        answer = 1;
-    }
-    else {
-        answer=slice/n;
-    }
     return answer;
 }
