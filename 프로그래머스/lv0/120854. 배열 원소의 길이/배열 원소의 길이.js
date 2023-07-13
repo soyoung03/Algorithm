d8d78d7fedeb19281984e6c2bbd147375a56c942
soyoung03 <128986073+soyoung03@users.noreply.github.com>
2023-07-14 02:02:42 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,10 @@
+function solution(strlist) {
+    var answer = [];
+    
+    for ( let i=0; i < strlist.length; i++)
+    {
+    answer.push(strlist[i].length);
+    }
+        
+     return answer;
+}
