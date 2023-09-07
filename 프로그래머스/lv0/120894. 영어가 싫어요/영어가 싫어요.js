86b814752e386c13d2380fc34eff655b672aabb6
soyoung03 <128986073+soyoung03@users.noreply.github.com>
2023-09-07 15:41:42 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i = 0; i < num.length; i++){
+     
+        numbers = numbers.replaceAll(num[i],i);
+    }
+    return Number(numbers)
+}
